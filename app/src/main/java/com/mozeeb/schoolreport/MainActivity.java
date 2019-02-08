a55bbd7dce60ff79c3c53df189204ec24c872f94
mujiburrohman <mujibnewbie5@gmail.com>
2019-02-08 10:32:25 +0700

    tgl 8 mujib ytang baru nahl

--- app/src/main/java/com/mozeeb/schoolreport/MainActivity.java
+++ app/src/main/java/com/mozeeb/schoolreport/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.mozeeb.schoolreport.user.NewsFragment;
@@ -21,6 +22,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar_semua);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("School Report");
+        toolbar.setSubtitle("IDN Boarding School");
+        toolbar.setLogo(R.drawable.ic_sign_out);
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
